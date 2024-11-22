68bf524b1f558d146302dfc8d23e6293b06265e1
Tatatadlisixti4 <tg.ibarra64@gmail.com>
2024-11-22 00:20:45 -0300

    Reemplazo local al remoto

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class RegisterController extends Controller
+{
+    public function index(){
+        return view('auth.register');
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required|max:30',
+            'username' => 'required|unique:users|min:3|max:20',
+            'email' => 'required|email|unique:users|max:60',
+            'password' => 'required'
+        ]);
+    }
+
+
+}
